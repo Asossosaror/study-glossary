90ac707d48e40476869189df689fb98fe7f56568
Asossosaror <111625299+Asossosaror@users.noreply.github.com>
2022-10-22 14:30:25 +0200

    createPair-function
    Started working on the createPair-function.

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-
+var newWord = '';
+var explanation = '';
+var list_newWords = [];
+var list_explanations = [];
+var obj_sets = {};
 
 function openPage(url){
     document.location.href = url;
+}
+
+function createPair(){
+    newWord = document.getElementById('textinput_newword').ariaValueText;
 }
