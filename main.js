16e77ef7a0b7ffdc80649469a240fb5672e61f39
Asossosaror <111625299+Asossosaror@users.noreply.github.com>
2022-10-25 20:00:33 +0200

    Save-function.
    I think I've fixed the creation of glossary sets, but I'm pushing to origin to see if it works in the browser.

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var newWord = '';
-var explanation = '';
-var list_newWords = [];
-var list_explanations = [];
+var newExplanation = '';
 var obj_sets = {};
 
 function openPage(url){
@@ -9,5 +7,22 @@ function openPage(url){
 }
 
 function createPair(){
-    newWord = document.getElementById('textinput_newword').ariaValueText;
+    newWord = document.getElementById('textinput_newword');
+    console.log(newWord.value);
+    newExplanation = document.getElementById('textinput_explanation');
+    console.log(newExplanation.value);
+    if(newWord.value != "" && newExplanation.value != ''){
+        obj_sets = JSON.parse(localStorage.getItem("obj_sets")) || {};
+        obj_sets[newWord.value] = newExplanation.value;
+        console.log(obj_sets);
+        obj_sets_serialized = JSON.stringify(obj_sets);
+        localStorage.setItem("obj_sets", obj_sets_serialized);
+        console.log(obj_sets['gg']);
+    }
+}
+
+function resetStorage(){
+    obj_sets_serialized = JSON.stringify({});
+    localStorage.setItem("obj_sets", obj_sets_serialized);
+    console.log(JSON.parse(localStorage.getItem("obj_sets")));
 }
