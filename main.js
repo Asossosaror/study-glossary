c6236c115e625567147fb5be3f5582ac5207f968
Asossosaror <111625299+Asossosaror@users.noreply.github.com>
2022-12-01 21:59:16 +0100

    Taking away debug-button.

--- main.js
+++ main.js
@@ -267,7 +267,3 @@ function another(){
     document.getElementById("testp2").innerHTML = Object.keys(cracked);
     console.log(Object.keys(cracked).includes("blabla"));
 }
-
-function blabla() {
-    document.getElementById("testing").value = "Nice";
-}
