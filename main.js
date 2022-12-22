53920b2dd1912f0a85a0fd1193105279fe53d899
Asossosaror <assar.grimlund@gmail.com>
2022-12-22 22:01:15 +0100

    Bug fix

--- main.js
+++ main.js
@@ -413,7 +413,7 @@ function generateDropdown() {
     for(i = 0; i < headlines.length(); i++) {
         let parentDiv = document.getElementById("dropdown-links1");
         let newATag = document.createElement("a");
-        newATag.innerText = headlines[i];
+        newATag.innerHTML = headlines[i];
         newATag.className = "dropdown-a-tag";
         newATag.id = "dropdown-a-tag" + String(i);
         newATag.href = "#";
